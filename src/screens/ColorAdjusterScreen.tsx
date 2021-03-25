f8ec9e3a68b3705c3d1f597b8cc00ac82ed4dd35
Luiz Dubas <luiz.dubas@podium.com>
2021-03-25 13:29:38 -0300

    Use reducer

--- src/screens/ColorAdjusterScreen.tsx
+++ src/screens/ColorAdjusterScreen.tsx
@@ -1,42 +1,57 @@
-import React, { useState } from "react";
+import React, { useState, useReducer } from "react";
 import { StyleSheet, Text, Button, SafeAreaView, View } from "react-native";
 import ColorCounter from "../components/ColorCounter";
 
 const COLOR_INCREMENT = 5;
 
-const ColorAdjusterScreen = () => {
-    const [redTint, setRedTint] = useState(0);
-    const [greenTint, setGreenTint] = useState(0);
-    const [blueTint, setBlueTint] = useState(0);
-
-    const setColor = (currentValue, operation, setValue) => {
-        currentValue >= 255 || currentValue <= 0 ? 
-            null : 
-            setValue(currentValue + operation);
-        return
+const reducer = (state, action) => {
+    const isChangeValid = (value, amount) => {
+        return value + amount >= 0 && value + amount <= 255;
     }
+    const { type, payload } = action;
+    switch (type) {
+        case 'change_red':
+            return isChangeValid(state.red, payload)
+                ? { ...state, red: state.red + payload } 
+                : state;
+        case 'change_green':
+            return isChangeValid(state.green, payload)
+                ? { ...state, green: state.green + payload } 
+                : state;
+        case 'change_blue':
+            return isChangeValid(state.blue, payload)
+                ? { ...state, blue: state.blue + payload } 
+                : state;
+        default:
+            return state
+    }
+}
+
+const ColorAdjusterScreen = () => {
+    const [state, dispatch] = useReducer(reducer, {red: 0, green: 0, blue: 0 });
+    const { red, green, blue } = state
 
     return (
         <SafeAreaView>
             <ColorCounter 
                 title="Red" 
-                value={redTint} 
-                onIncrease={() => { setColor(redTint, COLOR_INCREMENT, setRedTint) }}
-                onDecrease={() => { setColor(redTint, -COLOR_INCREMENT, setRedTint) }}
+                value={red} 
+                onIncrease={() => { dispatch({ type: 'change_red', payload: COLOR_INCREMENT }) }}
+                onDecrease={() => { dispatch({ type: 'change_red', payload: -COLOR_INCREMENT }) }}
             />
             <ColorCounter 
                 title="Green" 
-                value={greenTint} 
-                onIncrease={() => { setColor(greenTint, COLOR_INCREMENT, setGreenTint) }}
-                onDecrease={() => { setColor(greenTint, -COLOR_INCREMENT, setGreenTint) }}
+                value={green} 
+                onIncrease={() => { dispatch({ type: 'change_green', payload: COLOR_INCREMENT }) }}
+                onDecrease={() => { dispatch({ type: 'change_green', payload: -COLOR_INCREMENT }) }}
             />
             <ColorCounter 
                 title="Blue" 
-                value={blueTint} 
-                onIncrease={() => { setColor(blueTint, COLOR_INCREMENT, setBlueTint) }}
-                onDecrease={() => { setColor(blueTint, -COLOR_INCREMENT, setBlueTint) }}
+                value={blue} 
+                onIncrease={() => { dispatch({ type: 'change_blue', payload: COLOR_INCREMENT }) }}
+                onDecrease={() => { dispatch({ type: 'change_blue', payload: -COLOR_INCREMENT }) }}
             />
-            <View style={{ marginHorizontal: 12, height: 100, backgroundColor: `rgb(${redTint}, ${greenTint}, ${blueTint})`}}/>
+            <View style={{ marginHorizontal: 12, height: 100, backgroundColor: `rgb(${red}, ${green}, ${blue})`}}/>
         </SafeAreaView>
     );
 };
